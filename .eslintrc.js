64a6a1ab489908359be1e522cea9d3456542f1e9
Sean Spradlin <seanspradlin@gmail.com>
2021-03-18 14:51:21 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+    mocha: true,
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+  },
+};
