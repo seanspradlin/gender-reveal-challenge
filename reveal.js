11c93bc98b9f08114fe0224cb368bae67912f11b
Sean Spradlin <seanspradlin@gmail.com>
2021-03-19 10:19:14 -0500

    use http server to host file

--- reveal.js
+++ reveal.js
@@ -1,18 +1,46 @@
 const path = require('path');
 const querystring = require('querystring');
 const open = require('open');
+const fs = require('fs');
+const { URL } = require('url');
+const http = require('http');
 
-function generateFilepath() {
-  const root = path.join(__dirname, 'static', 'index.html');
-  const params = querystring.stringify({
-    token: 'foo',
+async function createServer(port) {
+  return new Promise((resolve, reject) => {
+    const server = http.createServer((req, res) => {
+      const url = new URL(req.url, `http://localhost:${port}`);
+      const filename = url.filepath || 'index.html';
+      const filepath = path.join(__dirname, 'static', filename);
+      fs.readFile(filepath, (error, data) => {
+        if (error) {
+          res.writeHead(404);
+          res.end(JSON.stringify(error));
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
+    });
+    server.listen(port, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(server);
+      }
+    });
   });
-  return `${root}?${params}`;
 }
 
-const filepath = generateFilepath();
+async function execute() {
+  const port = 8080;
+  await createServer(port);
+  const code = 123;
+  const qs = querystring.stringify({ gendercode: code });
+  const uri = `http://localhost:${port}/index.html?${qs}`;
+  await open(uri, { wait: true });
+}
 
-open(filepath).then(() => {
+execute().then(() => {
   console.log('Congratulations!');
 }).catch((err) => {
   console.error(err);
